6b4ffb5da9729964f224bb510dfcdfd0f25799e6
michi <michi@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2006-08-31 14:52:29 +0000

    delete directory recursively added

--- src/java/org/wyona/commons/io/FileUtil.java
+++ src/java/org/wyona/commons/io/FileUtil.java
@@ -88,4 +88,21 @@ public final class FileUtil {
 
         return fileName;
     }
+
+    /**
+     *
+     */
+    static public boolean deleteDirectory(File file) {
+        if(file.exists()) {
+            File[] files = file.listFiles();
+            for(int i = 0; i < files.length; i++) {
+                if(files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return file.delete();
+    }
 }
