2a77f4a4909c5965578ca4a8010ad78a79dd5512
guillaume <guillaume@db03cdf0-32eb-0310-9083-ccfe9ce60729>
2009-07-20 16:09:22 +0000

    Svn metadata aligned to the Yanel project.
    Issue: 6942
    Issue: 7164

--- build.bat
+++ build.bat
@@ -2,11 +2,11 @@
 
 rem ----- Ignore system ANT_HOME variable
 set ORIGINAL_ANT_HOME=%ANT_HOME%
-set ANT_HOME=tools\apache-ant-1.6.2
+set ANT_HOME=tools\apache-ant
 
 call %ANT_HOME%\bin\ant -version
-rem call %ANT_HOME%\bin\ant -f src\build\build.xml
-call %ANT_HOME%\bin\ant -f build.xml -Djava.endorsed.dirs=lib\endorsed -logger org.apache.tools.ant.NoBannerLogger -emacs %1 %2 %3 %4 %5 %6 %7 %8 %9
+rem call %ANT_HOME%\bin\ant -lib tools\apache-ant_extras -f src\build\build.xml
+call %ANT_HOME%\bin\ant -lib tools\apache-ant_extras -f build.xml -Djava.endorsed.dirs=lib\endorsed -logger org.apache.tools.ant.NoBannerLogger -emacs %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 rem ----- Restore ANT_HOME
 set ANT_HOME=%ORIGINAL_ANT_HOME%
